4895dcb0d29b9cfefdad6488992f73437c13cbd8
David <weedman950@gmail.com>
2017-12-04 23:20:17 -0800

    Added EEPROM Example

---
+++ Sheldon/EEPROM_Example/Debug/EEPROM_Example.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\David\AppData\Local\Temp\VMBuilds\EEPROM_Example\uno\Debug\EEPROM_Example.cpp.o: \
+ C:\Users\David\AppData\Local\Temp\VMBuilds\EEPROM_Example\uno\Debug\EEPROM_Example.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\EEPROM\src/EEPROM.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Community\Common7\IDE\Extensions\pbd1usl1.2ll\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
---
+++ Sheldon/EEPROM_Example/EEPROM_Example.ino
@@ -0,0 +1,73 @@
+/* EEPROM Test Program
+David Weed 12/4/17
+
+This program demonstrates the reading a writing of float data to the interal EEPROM.
+Tested using an Arduino\Genuino Uno with nothing connected.
+*/
+
+#include <EEPROM.h>
+
+template<class T>
+void eepromWrite(T data);
+void eepromReadAll();
+void eepromClear();
+
+//stores the next available address
+//starts at 4 because it takes up 0,1,2,3
+int nextAddress = 4;
+
+//whether you want to write data or read it we could move this to a phyisical switch to avoid code edits
+bool read = true;
+
+void setup()
+{
+	Serial.begin(9600);
+
+	if (read)
+	{
+		EEPROM.get(0, nextAddress);
+		eepromReadAll();
+	}
+	else
+	{
+		EEPROM.put(0, nextAddress);
+		EEPROM.get(0, nextAddress);
+		eepromWrite(3.141f);
+		eepromWrite(2.718f);
+		eepromWrite(1.234f);
+		eepromReadAll();
+	}
+}
+
+
+void loop()
+{
+	//nothing to loop
+}
+
+
+template<class T>
+void eepromWrite(T data)
+{
+	EEPROM.put(nextAddress, data);
+	nextAddress += sizeof(data);
+	EEPROM.put(0, nextAddress);
+}
+
+void eepromReadAll()
+{
+	float tempData;
+	for (int i = 4; i < nextAddress; i += 4)
+	{
+		EEPROM.get(i, tempData);
+		Serial.println(double(tempData), 3);
+	}
+}
+
+void eepromClear()
+{
+	for (int i = 0; i < EEPROM.length(); i++)
+	{
+		EEPROM.write(i, 0);
+	}
+}
